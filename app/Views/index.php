0c9c91cb97f2ead63afe282c37c70f70a6e6617b
CaibalLester <caiballester4@gmail.com>
2023-09-29 22:17:28 +0800

    main

---
+++ app/Views/index.php
@@ -0,0 +1,12 @@
+<?= $this->include('/inc/top') ?>
+
+<body>
+<?= $this->include('/inc/header') ?>
+<?= $this->include('/inc/section_1') ?>
+<?= $this->include('/inc/section_2') ?>
+<?= $this->include('/inc/section_3') ?>
+<?= $this->include('/inc/section_4') ?>
+<?= $this->include('/inc/section_5') ?>
+<?= $this->include('/inc/footer') ?>
+<?= $this->include('/inc/myjs') ?>
+</body>
